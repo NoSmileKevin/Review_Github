355ac84d1abda0f093f0b8ebce3856a29ff46d35
Qichen vwmare <yanqichen1993@hotmail.com>
2018-07-05 14:16:25 +0800

    modify bug

--- hello.c
+++ hello.c
@@ -2,4 +2,9 @@
 void main(){
 	printf("Hello World\n");
 	printf("There is only one World\n");
+	int class_num = 288;
+	char my_array[10];
+	char another_array[10] = "Goodbye";
+	sprintf(my_array, "Hello CPRE %d", class_num);
+	printf("%s", another_array);
 }
